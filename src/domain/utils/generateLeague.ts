4603441e257846128062c8d452f9cdc284158215
Daniel Ruiz <danielruimar@gmail.com>
2023-06-20 23:29:11 +0200

    Fix problems

--- src/domain/utils/generateLeague.ts
+++ src/domain/utils/generateLeague.ts
@@ -1,12 +1,19 @@
 import { IMatchCreate } from "../entities/match-entity";
 import { Team } from "../entities/team-entity";
+import { ROL, User } from "../entities/user-entity";
 import { matchOdm } from "../odm/match.odm";
+import { generateGoalIds } from "./generateGoals";
 
 export const generateLeague = async (): Promise<void> => {
   try {
     const teams = await Team.find();
     if (teams.length === 0) {
       console.error("No hay equipos en la BBDD.");
+      return;
+    }
+    const players = await User.find({ rol: ROL.PLAYER, team: { $in: teams.map(team => team.id) } });
+    if (players.length === 0) {
+      console.error("No hay jugadores en la BBDD.");
       return;
     }
     await matchOdm.deleteAllMatch();
@@ -34,12 +41,20 @@ export const generateLeague = async (): Promise<void> => {
         const localTeam = teams[home];
         const visitorTeam = teams[away];
 
+        const localPlayers = await User.find({ team: localTeam.id }).populate("team");
+        const visitorPlayers = await User.find({ team: visitorTeam.id }).populate("team");
+
         const matchDate: Date = new Date(startDate.getTime() + round * 7 * 24 * 60 * 60 * 1000);
+
+        const localGoals = generateGoalIds(localPlayers, 0, 5);
+        const visitorGoals = generateGoalIds(visitorPlayers, 0, 5);
 
         const match: IMatchCreate = {
           date: matchDate,
           localTeam,
           visitorTeam,
+          goalsLocal: localGoals,
+          goalsVisitor: visitorGoals,
           played: false,
           round: round + 1, // Se incrementa en 1 para indicar la ronda actual
         };
@@ -67,12 +82,20 @@ export const generateLeague = async (): Promise<void> => {
         const localTeam = teams[home];
         const visitorTeam = teams[away];
 
-        const matchDate: Date = new Date(startDate.getTime() + round * 7 * 24 * 60 * 60 * 1000);
+        const localPlayers = await User.find({ team: localTeam.id }).populate("team");
+        const visitorPlayers = await User.find({ team: visitorTeam.id }).populate("team");
+
+        const matchDate: Date = new Date(startDate.getTime() + (round + numRoundsPerFase) * 7 * 24 * 60 * 60 * 1000);
+
+        const localGoals = generateGoalIds(localPlayers, 0, 5);
+        const visitorGoals = generateGoalIds(visitorPlayers, 0, 5);
 
         const match: IMatchCreate = {
           date: matchDate,
           localTeam,
           visitorTeam,
+          goalsLocal: localGoals,
+          goalsVisitor: visitorGoals,
           played: false,
           round: round + numRoundsPerFase + 1, // Se incrementa en 1 para indicar la ronda actual
         };
@@ -88,7 +111,7 @@ export const generateLeague = async (): Promise<void> => {
     const matchSort = matches.sort((a, b) => a.round - b.round)
     for (let i = 0; i < matchSort.length; i++) {
       const match = matches[i];
-      console.log(`Jornada ${match.round} Partido: ${match.localTeam.name}/ ${match.visitorTeam.name} Fecha ${match.date.getDate()}/${match.date.getMonth()}`)
+      console.log(`Jornada ${match.round} - Partido: ${match.localTeam.name} / ${match.visitorTeam.name} - Resultado: ${match.goalsLocal?.length ? match.goalsLocal?.length : 0}-${match.goalsVisitor?.length ? match.goalsVisitor?.length : 0} Fecha ${match.date.getDate()}/${match.date.getMonth()}`)
     }
     console.log("Partidos generados correctamente");
     console.log({
